23e0950b0cffc1569a38a29b13c281dc544b2984
JavierBurgara <javierburgara150@yahoo.com>
2023-11-09 22:57:36 -0800

    fix code you install jate and install button is working

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,5 +1,5 @@
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
@@ -39,3 +39,54 @@ registerRoute(
     ],
   })
 );
+// // Vanilla service worker (no workbox)
+// const CACHE_NAME = 'cache-v1';
+// const urlsToCache = [
+//   '/',
+//   '/index.html',
+//   '/css/style.css',
+//   '/index.js',
+//   'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css',
+//   'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js',
+//   '/images/icon.png',
+// ];
+
+// // The three phases of the service worker's life cycle are:
+// // 1. install
+// // 2. activate
+// // 3. claim
+
+// // Install - the service worker is first installed and then activated.
+// self.addEventListener('install', (e) =>
+//   e.waitUntil(
+//     caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+//   )
+// );
+
+// // Activate - the service worker is activated after install.
+// self.addEventListener('activate', (e) =>
+//   e.waitUntil(
+//     caches.keys().then((keyList) =>
+//       Promise.all(
+//         keyList.map((key) => {
+//           if (key !== CACHE_NAME) {
+//             return caches.delete(key);
+//           }
+//         })
+//       )
+//     )
+//   )
+// );
+
+// // Claim - the service worker is claimed after install.
+// // When a service worker is initially registered, pages won't use it until the next load.
+// // The clients.claim() method is used to claim the service worker immediately.
+// self.addEventListener('activate', (e) => {
+//   e.waitUntil(clients.claim());
+// });
+
+// // Example of a simple cache-first network-first strategy
+// // The service worker is checking the cache for a response and if it doesn't find it, it fetches it.
+// self.addEventListener('fetch', (e) =>
+//   e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)))
+// );
--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,51 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('Putting to the database');
+
+  // Create a connection to the database database and version we want to use.
+  const jateDB = await openDB('jate', 1);
+
+  // Create a new transaction and specify the database and data privileges.
+  const tx = jateDB.transaction('jate', 'readwrite');
+
+  // Open up the desired object store.
+  const store = tx.objectStore('jate');
+
+  // Use the .add() method on the store and pass in the content.
+  const request = store.put({  id:1, value: content });
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log('Data saved to the database');
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () =>
+{
+  console.log('getting to the database');
+
+  // Create a connection to the database database and version we want to use.
+  const jateDB = await openDB('jate', 1);
+
+  // Create a new transaction and specify the database and data privileges.
+  const tx = jateDB.transaction('jate', 'readonly');
+
+  // Open up the desired object store.
+  const store = tx.objectStore('jate');
+
+  // Use the .add() method on the store and pass in the content.
+  const request = store.get(1);
+
+  // Get confirmation of the request.
+  const result = await request;
+  if (result){
+    console.log('printed into the database')
+  } else {
+    console.log('result not found')
+  } 
+  return result?.value
+};
 
 initdb();
